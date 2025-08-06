4d613913f03f5408fbd077db33b71a840dbae089
Nabeel Okal <nabeel.okal@gmail.com>
2025-08-06 14:08:30 +0300

    Initial commit.

---
+++ build/Desktop_Qt_6_9_1-Debug/ui_dialog.h
@@ -0,0 +1,151 @@
+/********************************************************************************
+** Form generated from reading UI file 'dialog.ui'
+**
+** Created by: Qt User Interface Compiler version 6.9.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_DIALOG_H
+#define UI_DIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAbstractButton>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QVBoxLayout>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Dialog
+{
+public:
+    QVBoxLayout *verticalLayout;
+    QPlainTextEdit *plainTextEdit;
+    QDialogButtonBox *buttonBox;
+
+    void setupUi(QDialog *Dialog)
+    {
+        if (Dialog->objectName().isEmpty())
+            Dialog->setObjectName("Dialog");
+        Dialog->resize(633, 346);
+        QSizePolicy sizePolicy(QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Preferred);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(Dialog->sizePolicy().hasHeightForWidth());
+        Dialog->setSizePolicy(sizePolicy);
+        QPalette palette;
+        QBrush brush(QColor(0, 0, 0, 255));
+        brush.setStyle(Qt::BrushStyle::SolidPattern);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::WindowText, brush);
+        QBrush brush1(QColor(255, 255, 255, 255));
+        brush1.setStyle(Qt::BrushStyle::SolidPattern);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Button, brush1);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Light, brush1);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Midlight, brush1);
+        QBrush brush2(QColor(127, 127, 127, 255));
+        brush2.setStyle(Qt::BrushStyle::SolidPattern);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Dark, brush2);
+        QBrush brush3(QColor(170, 170, 170, 255));
+        brush3.setStyle(Qt::BrushStyle::SolidPattern);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Mid, brush3);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Text, brush);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::BrightText, brush1);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::ButtonText, brush);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Base, brush1);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Window, brush1);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Shadow, brush);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::AlternateBase, brush1);
+        QBrush brush4(QColor(255, 255, 220, 255));
+        brush4.setStyle(Qt::BrushStyle::SolidPattern);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::ToolTipBase, brush4);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::ToolTipText, brush);
+        QBrush brush5(QColor(0, 0, 0, 127));
+        brush5.setStyle(Qt::BrushStyle::SolidPattern);
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::PlaceholderText, brush5);
+#endif
+#if QT_VERSION >= QT_VERSION_CHECK(6, 6, 0)
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Accent, brush1);
+#endif
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::WindowText, brush);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Button, brush1);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Light, brush1);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Midlight, brush1);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Dark, brush2);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Mid, brush3);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Text, brush);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::BrightText, brush1);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::ButtonText, brush);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Base, brush1);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Window, brush1);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Shadow, brush);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::AlternateBase, brush1);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::ToolTipBase, brush4);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::ToolTipText, brush);
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::PlaceholderText, brush5);
+#endif
+#if QT_VERSION >= QT_VERSION_CHECK(6, 6, 0)
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Accent, brush1);
+#endif
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::WindowText, brush2);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Button, brush1);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Light, brush1);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Midlight, brush1);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Dark, brush2);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Mid, brush3);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Text, brush2);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::BrightText, brush1);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::ButtonText, brush2);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Base, brush1);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Window, brush1);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Shadow, brush);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::AlternateBase, brush1);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::ToolTipBase, brush4);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::ToolTipText, brush);
+        QBrush brush6(QColor(127, 127, 127, 127));
+        brush6.setStyle(Qt::BrushStyle::SolidPattern);
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::PlaceholderText, brush6);
+#endif
+#if QT_VERSION >= QT_VERSION_CHECK(6, 6, 0)
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Accent, brush1);
+#endif
+        Dialog->setPalette(palette);
+        Dialog->setStyleSheet(QString::fromUtf8(""));
+        verticalLayout = new QVBoxLayout(Dialog);
+        verticalLayout->setObjectName("verticalLayout");
+        plainTextEdit = new QPlainTextEdit(Dialog);
+        plainTextEdit->setObjectName("plainTextEdit");
+
+        verticalLayout->addWidget(plainTextEdit);
+
+        buttonBox = new QDialogButtonBox(Dialog);
+        buttonBox->setObjectName("buttonBox");
+        buttonBox->setStandardButtons(QDialogButtonBox::StandardButton::Open|QDialogButtonBox::StandardButton::Save);
+
+        verticalLayout->addWidget(buttonBox);
+
+
+        retranslateUi(Dialog);
+
+        QMetaObject::connectSlotsByName(Dialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *Dialog)
+    {
+        Dialog->setWindowTitle(QCoreApplication::translate("Dialog", "Assignment 3", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Dialog: public Ui_Dialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_DIALOG_H
---
+++ dialog.cpp
@@ -0,0 +1,101 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+
+    connect(ui->buttonBox, &QDialogButtonBox::clicked, this, &Dialog::on_buttonBox_clicked);
+
+    // Creating 5 more push buttons
+
+    QPushButton *btnCopy = new QPushButton("Copy", this);
+    QPushButton *btnCut = new QPushButton("Cut", this);
+    QPushButton *btnPaste = new QPushButton("Paste", this);
+    QPushButton *btnUndo = new QPushButton("Undo", this);
+    QPushButton *btnRedo = new QPushButton("Redo", this);
+
+    // Adding them to the buttonBox on the  ui file
+
+    ui->buttonBox->addButton(btnCopy, QDialogButtonBox::ActionRole);
+    ui->buttonBox->addButton(btnCut, QDialogButtonBox::ActionRole);
+    ui->buttonBox->addButton(btnPaste, QDialogButtonBox::ActionRole);
+    ui->buttonBox->addButton(btnUndo, QDialogButtonBox::ActionRole);
+    ui->buttonBox->addButton(btnRedo, QDialogButtonBox::ActionRole);
+
+    // Creating their functionalities
+
+    connect(btnCopy, &QPushButton::clicked, ui->plainTextEdit, &QPlainTextEdit::copy);
+    connect(btnCut, &QPushButton::clicked, ui->plainTextEdit, &QPlainTextEdit::cut);
+    connect(btnPaste, &QPushButton::clicked, ui->plainTextEdit, &QPlainTextEdit::paste);
+    connect(btnUndo, &QPushButton::clicked, ui->plainTextEdit, &QPlainTextEdit::undo);
+    connect(btnRedo, &QPushButton::clicked, ui->plainTextEdit, &QPlainTextEdit::redo);
+
+    load();
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+void Dialog::on_plainTextEdit_textChanged() {
+    m_saved = false;
+}
+
+
+void Dialog::on_buttonBox_clicked(QAbstractButton *button) {
+    QDialogButtonBox::StandardButton stdBtn = ui->buttonBox->standardButton(button);
+
+    if(stdBtn == QDialogButtonBox::Open){
+        qDebug() << "You pressed Open.";
+        load();
+        return;
+
+    }
+
+    if(stdBtn == QDialogButtonBox::Save){
+        qDebug() << "You pressed Save.";
+        save();
+        return;
+    }
+}
+
+void Dialog::load() {
+    ui->plainTextEdit->clear();
+    QFile file("test_file.txt");
+
+    if(!file.exists()) return;
+
+    if(!file.open(QIODevice::ReadOnly)) {
+        QMessageBox::critical(this, "Error", file.errorString());
+        return;
+    }
+
+    QTextStream stream(&file);
+    ui->plainTextEdit->setPlainText(stream.readAll());
+
+    file.close();
+    m_saved = true;
+}
+
+void Dialog::save() {
+    QFile file("test_file.txt");
+
+    if(!file.open(QIODevice::WriteOnly)){
+        QMessageBox::critical(this, "ERROR!", file.errorString());
+        return;
+    }
+
+    QTextStream stream(&file);
+    stream << ui->plainTextEdit->toPlainText();
+
+    file.close();
+    m_saved = true;
+}
+
+void Dialog::closeEvent(QCloseEvent *event) {
+    event->accept();
+}
+
